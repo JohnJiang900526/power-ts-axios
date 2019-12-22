56696a96864b662bebdfe355ff2863ea298503dc
JohnJiang900526 <jiangangshjd@163.com>
2019-12-22 23:05:37 +0800

    res

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -92,6 +92,16 @@ import axios from '../../src/index'
 //   data: searchParams
 // });
 
+axios({
+  method: 'post',
+  url: '/base/post',
+  data: {
+    a: 1,
+    b: 2
+  }
+}).then((res) => {
+  console.log(res);
+});
 
 axios({
   method: 'post',
