642e477e31dcb5ed4e5b0bb3d5c7da669b5f89d4
JohnJiang900526 <jiangangshjd@163.com>
2019-12-24 10:59:28 +0800

    power-ts-axios 错误信息处理

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,14 +1,26 @@
 import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from "./types";
 import { parseHeaders } from "./helpers/headers";
+import { createError } from "./helpers/error";
 
 export default function xhr(config: AxiosRequestConfig):AxiosPromise {
   return new Promise((resolve, reject) => {
-    const { data = null, url, method = 'get', headers = {}, responseType }  = config;
+    const { 
+      data = null, 
+      url, 
+      method = 'get', 
+      headers = {}, 
+      responseType,
+      timeout
+    }  = config;
 
     const request = new XMLHttpRequest();
 
     if (responseType) {
       request.responseType = responseType;
+    }
+
+    if (timeout) {
+      request.timeout = timeout;
     }
 
     request.open(method.toUpperCase(), url, true);
@@ -17,6 +29,12 @@ export default function xhr(config: AxiosRequestConfig):AxiosPromise {
       if (request.readyState !== 4) {
         return;
       }
+
+      if (request.status === 0) {
+        return;
+      }
+
+
 
       const responseHeaders = parseHeaders(request.getAllResponseHeaders());
       const responseData = responseType !== 'text' ? request.response : request.responseText;
@@ -30,7 +48,23 @@ export default function xhr(config: AxiosRequestConfig):AxiosPromise {
         request
       };
 
-      resolve(response);
+      handleReponse(response);
+    };
+
+    function handleReponse(response: AxiosResponse) {
+      if (response.status >= 200 && response.status < 300) {
+        resolve(response);
+      } else {
+        reject(createError(`Request failed with status code ${response.status}`, config, null, request));
+      }
+    }
+
+    request.onerror = function handleError() {
+      reject(createError('Network Error', config, null, request));
+    };
+
+    request.ontimeout = function handleTimeout() {
+      reject(createError(`Timeout of ${timeout} ms exceeeded`, config, 'ECONNABORTED', request));
     };
 
     Object.keys(headers).forEach((name) => {
