f117ad1d9634c62427528e2aa7abaf12293eef80
JohnJiang900526 <jiangangshjd@163.com>
2019-12-26 23:06:01 +0800

    实现cancel方法

--- src/axios.ts
+++ src/axios.ts
@@ -3,6 +3,8 @@ import Axios from "./core/Axios";
 import { extend } from "./helpers/util";
 import defaults from "./defaults";
 import mergeConfig from "./core/mergeConfig";
+import Cancel, { isCancel } from "./cancel/cancel";
+import CancelToken from "./cancel/cancelToken";
 
 function createInstance(config: AxiosRequestConfig): AxiosStatic {
   const context = new Axios(config);
@@ -17,6 +19,10 @@ const axios = createInstance(defaults);
 
 axios.create = function create(config) {
   return createInstance(mergeConfig(defaults, config));
-}
+};
+
+axios.CancelToken = CancelToken;
+axios.Cancel = Cancel;
+axios.isCancel = isCancel;
 
 export default axios;
