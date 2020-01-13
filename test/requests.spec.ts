bc342d0cc41f884e7f736d9baf90b97454852aeb
JohnJiang900526 <jiangangshjd@163.com>
2020-01-13 15:18:43 +0800

    请求模块单元测试

---
+++ test/requests.spec.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosResponse, AxiosError } from '../src/index'
+import { getAjaxRequest } from './helper'
+
+describe('requests', () => {
+  beforeEach(() => {
+    jasmine.Ajax.install()
+  })
+
+  afterEach(() => {
+    jasmine.Ajax.uninstall()
+  })
+
+  test('should treat single string arg as url', () => {
+    axios('/foo')
+
+    return getAjaxRequest().then(request => {
+      expect(request.url).toBe('/foo')
+      expect(request.method).toBe('GET')
+    });
+  })
+})
+
