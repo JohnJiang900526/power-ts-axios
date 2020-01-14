029e9900cf6f848206b9b165fc0282b2f0cf1ca3
JohnJiang900526 <jiangangshjd@163.com>
2020-01-14 22:39:50 +0800

    mergeConfig 模块单元测试

--- src/core/mergeConfig.ts
+++ src/core/mergeConfig.ts
@@ -1,43 +1,46 @@
-import { AxiosRequestConfig } from "../types";
-import { isPlainObject, deepMerge } from "../helpers/util";
+import { AxiosRequestConfig } from '../types'
+import { isPlainObject, deepMerge } from '../helpers/util'
 
-const strats = Object.create(null);
+const strats = Object.create(null)
 
 function defaultStrat(val1: any, val2: any): any {
-  return typeof val2 !== "undefined" ? val2: val1;
+  return typeof val2 !== 'undefined' ? val2 : val1
 }
 
 function fromVal2Strat(val1: any, val2: any): any {
-  if (typeof val2 !== "undefined") {
-    return val2;
+  if (typeof val2 !== 'undefined') {
+    return val2
   }
 }
 
-function deepMergeStrat (val1: any, val2: any): any {
+function deepMergeStrat(val1: any, val2: any): any {
   if (isPlainObject(val2)) {
-    return deepMerge(val1, val2);
-  } else if (typeof val2 !== "undefined") {
-    return val2;
-  } else if(isPlainObject(val1)) {
-    return deepMerge(val1);
-  } else if (typeof val1 !== "undefined") {
-    return val1;
+    return deepMerge(val1, val2)
+  } else if (typeof val2 !== 'undefined') {
+    return val2
+  } else if (isPlainObject(val1)) {
+    return deepMerge(val1)
+  } else {
+    return val1
   }
 }
 
-const stratKeysFromVal2 = ["url", "parmas", "data"];
+const stratKeysFromVal2 = ['url', 'parmas', 'data']
 
-stratKeysFromVal2.forEach((key) => {
-  strats[key] = fromVal2Strat;
-});
+stratKeysFromVal2.forEach(key => {
+  strats[key] = fromVal2Strat
+})
 
-const stratKeysDeepMerge = ["headers", "auth"];
+const stratKeysDeepMerge = ['headers', 'auth']
 
 stratKeysDeepMerge.forEach(key => {
-  strats[key] = deepMergeStrat;
-});
+  strats[key] = deepMergeStrat
+})
 
-export default function mergeConfig (config1: AxiosRequestConfig, config2?: AxiosRequestConfig): AxiosRequestConfig {
+export default function mergeConfig(
+  config1: AxiosRequestConfig,
+  config2?: AxiosRequestConfig
+): AxiosRequestConfig {
   if (!config2) {
     config2 = {}
   }
@@ -55,10 +58,10 @@ export default function mergeConfig (config1: AxiosRequestConfig, config2?: Axio
   }
 
   function mergeField(key: string): void {
-    const strat = strats[key] || defaultStrat;
+    const strat = strats[key] || defaultStrat
 
-    config[key] = strat(config1[key], config2![key]);
+    config[key] = strat(config1[key], config2![key])
   }
 
-  return config;
+  return config
 }
